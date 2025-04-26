3c7014ceaaada4e76689a558fdeafbaa3d1e293e
seoywa <132980713+seoywa@users.noreply.github.com>
2025-04-26 13:33:52 +0700

    Finding errors

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -1,7 +1,7 @@
-import config from '@/lib/config'
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import config from "@/lib/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
-const sql = neon(config.env.databaseUrl)
+const sql = neon(config.env.databaseUrl);
 
-export const db = drizzle({ client: sql });
+export const db = drizzle({ client: sql, casing: "snake_case" });
